def3b1137cbd1d58a9d4aaa8783dff80e383a86b
Bert Jan van der Steeg <bertjan@Berts-MacBook-Pro.local>
2019-04-15 13:39:12 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi there again! How are you?
           </p>
           <a
             className="App-link"
